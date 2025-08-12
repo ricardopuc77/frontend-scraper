2291ed45b72d4dd4225fe81d71cb842feb4a0083
Ricardo Puc <ricardopuc77@gmail.com>
2025-08-12 15:49:13 -0600

    mostrar todos los componentes creados en app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import './App.css'
+import { useState } from "react";
+import FilterBar from "./components/FilterBar";
+import DataTable from "./components/DataTable";
+import type { Option, RowItem } from "./types";
+import api from "./services/api";
 
-function App() {
+export default function App() {
+
+  const [areas] = useState<Option[]>([]);
+  const [instituciones] = useState<Option[]>([]);
+  const [puestos] = useState<Option[]>([]);
+  const [areaId, setAreaId] = useState("");
+  const [instId, setInstId] = useState("");
+  const [puestoId, setPuestoId] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [rows, setRows] = useState<RowItem[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState<string | null>(null);
+
+  const handleSearch = async () => {
+
+  };
 
   return (
-    <div className="min-h-dvh flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Proyecto Filtros + Tabla</h1>
-    </div>
-  )
-}
+    <main className="max-w-6xl mx-auto p-6 space-y-6">
+      <header>
+        <h1 className="text-2xl font-bold">Funcionarios</h1>
+        <p className="text-gray-600 text-sm">Filtra por Área, Institución y Puesto</p>
+      </header>
 
-export default App
+      <FilterBar
+        areaId={areaId} setAreaId={setAreaId} areas={areas}
+        instId={instId} setInstId={setInstId} instituciones={instituciones}
+        puestoId={puestoId} setPuestoId={setPuestoId} puestos={puestos}
+        nombre={nombre} setNombre={setNombre}
+        onSearch={() => console.log("Buscar")}
+      />
+      <DataTable rows={rows} />
+    </main>
+  );
+}
