251772dd53368b09c53f050d97d6ff9e9739ad62
Ricardo Puc <ricardopuc77@gmail.com>
2025-08-12 15:25:14 -0600

    front initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-dvh flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Proyecto Filtros + Tabla</h1>
+    </div>
+  )
+}
+
+export default App
