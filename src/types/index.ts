da2cfc347b33195c2d1555090b0ca00aa7c339f5
Ricardo Puc <ricardopuc77@gmail.com>
2025-08-12 15:48:45 -0600

    configuracion para hacer peticiones al back con axios

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type Option = { id: number | string; nombre: string };
+
+export type RowItem = {
+  id: number | string;
+  nombre: string;
+  area: string;
+  institucion: string;
+  puesto: string;
+};
